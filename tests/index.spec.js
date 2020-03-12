3bef8f86fff97c013b024c5e44e61fe4aa259f75
Fea-Sin <1210992614@qq.com>
2020-03-12 17:19:52 +0800

    add test suite

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -3,8 +3,17 @@ import { mount, render } from 'enzyme';
 import { renderToJson } from 'enzyme-to-json';
 import Comp from '../src';
 
-describe('cr-comp', () => {
+describe('zfe', () => {
   it('base test comp', () => {
-    // expect('HELLO COMP').toBe('HELLO COMP')
+    const wrapper = mount(
+      <Comp />
+    )
+    expect('COMP IS DONE').toBe('COMP IS DONE')
+  })
+  it('snapshot to match', () => {
+    const wrapper = render(
+      <Comp />
+    )
+    expect( renderToJson(wrapper) ).toMatchSnapshot()
   })
 })
