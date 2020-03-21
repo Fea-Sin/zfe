2af646923c1366f4cea290ff4490929a7715c4c2
Fea-Sin <1210992614@qq.com>
2020-03-21 11:42:02 +0800

    force render

--- src/SCATTER/index.js
+++ src/SCATTER/index.js
@@ -20,67 +20,10 @@ class App extends React.Component {
 
   init = () => {
     const { option={} } = this.props
-    this.scatter = echarts.init(this.chart.current)
-    // let option = {
-    //   xAxis: {},
-    //   yAxis: {},
-    //   tooltip: {
-    //     formatter: 'Group {a}: ({c})'
-    //   },
-    //   series: [{
-    //       symbolSize: 10,
-    //       data: [
-    //         [10.0, 8.04],
-    //         [8.0, 6.95],
-    //         [13.0, 7.58],
-    //         [9.0, 8.81],
-    //         [11.0, 8.33],
-    //         [14.0, 9.96],
-    //         [6.0, 7.24],
-    //         [4.0, 4.26],
-    //         [12.0, 10.84],
-    //         [7.0, 4.82],
-    //         [5.0, 5.68]
-    //       ],
-    //       type: 'scatter',
-    //       markLine: {
-    //         animation: false,
-    //         label: {
-    //             formatter: 'y = 0.5 * x + 3',
-    //             position: 'middle'
-    //         },
-    //         lineStyle: {
-    //             type: 'solid'
-    //         },
-    //         tooltip: {
-    //             formatter: 'y = 0.5 * x + 3'
-    //         },
-    //         data: [
-    //           // [
-    //           //     {
-    //           //         // 起点和终点的项会共用一个 name
-    //           //         name: '最小值到最大值',
-    //           //         type: 'min'
-    //           //     },
-    //           //     {
-    //           //         type: 'max'
-    //           //     }
-    //           // ],
-    //           [
-    //             {
-    //                 name: '两个坐标之间的标线',
-    //                 coord: [4.0, 4.26]
-    //             },
-    //             {
-    //                 coord: [14.0, 9.96]
-    //             }
-    //           ]                                   
-    //         ]        
-    //       }
-    //   }]      
-    // }
-
-    this.scatter.setOption(option)
+    if (this.chart.current) {
+      this.scatter = echarts.init(this.chart.current)
+      this.scatter.setOption(option)
+    }
   }
 
   onResizeHandle = () => {
@@ -102,6 +45,7 @@ class App extends React.Component {
   
   render() {
     const { width, height } = this.props
+    this.init()
     return (
       <div ref={this.chart} style={{ width, height }}>
       </div>
