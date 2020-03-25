749ffac2c73fcf697afc1005c2c09bb07898d751
Fea-Sin <1210992614@qq.com>
2020-03-25 13:56:25 +0800

    add more item

--- examples/scatter.js
+++ examples/scatter.js
@@ -20,7 +20,7 @@ const option = {
       // console.log('what is this----', params)
       var res = '<div>' + params.data[0].toFixed(2) + ' , ' + params.data[1].toFixed(2) + '</div>'
       return res
-    }, 
+    },
     backgroundColor: 'rgba(250,250,250,1)',
     borderColor: '#e3e2e1',
     borderWidth: 1,
@@ -29,9 +29,9 @@ const option = {
       color: '#000',
     }
   },
-  color: ['#1890FF'],
-  series: [{
-      name: '散点图',
+  series: [
+    {
+      name: '散点图1',
       type: 'scatter',
       symbolSize: 10,
       data: [
@@ -47,6 +47,9 @@ const option = {
         [7.0, 4.82],
         [5.0, 5.68]
       ],
+      itemStyle: {
+        color: '#1890FF',
+      },
       markLine: {
         animation: false,
         symbol: 'none',
@@ -64,28 +67,31 @@ const option = {
             formatter: 'y = 0.5 * x + 3'
         },
         data: [
-          // [
-          //     {
-          //         // 起点和终点的项会共用一个 name
-          //         name: '最小值到最大值',
-          //         type: 'min'
-          //     },
-          //     {
-          //         type: 'max'
-          //     }
-          // ],
           [
             {
                 name: '两个坐标之间的标线',
-                coord: [4.0, 4.26]
+                coord: [5.0, 4.26]
             },
             {
-                coord: [14.0, 9.96]
+                coord: [12.0, 9.96]
             }
           ]                                   
         ]        
       }
-  }]    
+  },
+  {
+    name: '散点图2',
+    type: 'scatter',
+    symbolSize: 2,
+    data: [
+      [5.0, 4.26],
+      [12.0, 9.96],
+    ],
+    itemStyle: {
+      color: '#FF516A',
+    },       
+  }
+]    
 }
 
 function render(container) {
